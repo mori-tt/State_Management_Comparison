ea8b9d61eba3eb50f7507f94a678a8943bbe0a17
T_Mori <morit0301@gmail.com>
2024-06-21 21:16:20 +0900

    🐛 CSSファイル削除

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./App";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
